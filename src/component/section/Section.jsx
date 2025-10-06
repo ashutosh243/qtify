9520895475188aeb593256b53fe66a5c48625b34
ashutoshgupt243 <vnsashutoshit.gmail.com>
2025-10-06 16:03:01 +0530

    top and new album updated

--- src/component/section/Section.jsx
+++ src/component/section/Section.jsx
@@ -6,25 +6,23 @@ import { useState } from 'react';
 
 
 const Section = ({ heading, data }) => {
-  console.log(data);
   const [showAll, setShowAll] = useState(false);
-  const visibleAlbums = showAll ? data : data;
-  console.log(visibleAlbums);
+  const visibleAlbums = showAll ? Array.from(data) : Array.from(data).slice(0,7);
+
   return (<>
     <div className={Style.sections}>
       <div className={Style.heading}>
-          <p>{heading}</p>
-          <div>
-          {
-            data?.songs?.length > 4 && (
-              <button
-                className={Style.toggleBtn}
-                onClick={() => setShowAll(!showAll)}
-              >
-                {showAll ? 'Collapse' : 'Show all'}
-              </button>)
-          }
-          </div>
+        <p>{heading}</p>
+        <div>
+
+          <button
+            className={Style.toggleBtn}
+            onClick={() => setShowAll(!showAll)}
+          >
+            {showAll ? 'Collapse' : 'Show all'}
+          </button>
+
+        </div>
       </div>
       <div className={Style.container}>
         {Array.from(visibleAlbums)?.map((album) => { return <Card key={album.id} img={album.image} follow={album.follows}></Card> })}
